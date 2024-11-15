1068f492a54140650eec98b179d1800013ad4890
khoa9894 <khicondangyeu490@gmail.com>
2024-11-15 15:49:40 +0700

    add asset.js

---
+++ ssps/src/assets/asset.js
@@ -0,0 +1,4 @@
+import logo from '../assets/Icon.png';
+export const asset={
+logo,
+}
---
+++ ssps/src/component/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { asset } from '../assets/asset'
+
+function Navbar() {
+
+  return (
+    <div>
+      <img src={asset.logo} alt="" />
+    </div>
+  )
+}
+
+export default Navbar
