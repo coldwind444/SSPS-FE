2e49793f31cd17b9211742390ab9a369a1999b6e
khoa9894 <khicondangyeu490@gmail.com>
2024-11-15 19:41:49 +0700

    NavigationBar

--- ssps/src/assets/asset.js
+++ ssps/src/assets/asset.js
@@ -1,4 +1,8 @@
 import logo from '../assets/Icon.png';
+import notif from '../assets/Notification.png'
+import arrow from '../assets/Expand Arrow.png'
 export const asset={
+notif,
 logo,
+arrow,
 }
