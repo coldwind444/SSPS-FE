c6e9280eb0bcc01b33e914602de021092e60e12c
coldwind444 <minhtan14042004@gmail.com>
2024-11-18 15:04:58 +0700

    use logobachkhoa.png as page icon

--- ssps/src/assets/asset.js
+++ ssps/src/assets/asset.js
@@ -1,4 +1,4 @@
-import logo from '../assets/Icon.png';
+import logo from '../assets/logobachkhoa.png';
 import notif from '../assets/Notification.png'
 import arrow from '../assets/Expand Arrow.png'
 export const asset={
