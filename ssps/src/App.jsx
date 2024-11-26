7638b859e0c92ac76eff4576e4071f1ef5fc2de5
Hoangth1506 <thhoang150604@gmail.com>
2024-11-26 18:32:22 +0700

    thêm branch PrinterManagement

--- ssps/src/App.jsx
+++ ssps/src/App.jsx
@@ -7,18 +7,20 @@ import AuthLayout from './component/Layout/AuthLayout/AuthLayout'
 import PrintingPage from './pages/student/PrintingPage/PrintingPage'
 import HistoryPage from './pages/Student_PrintHistory/PrintHistory'
 import Purchase from './pages/PagePurchase/Purchase'
+import PrinterManagement from './pages/PrinterManagement/PrinterManagement'
 import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className='app'>
         <Routes>
-          <Route path='/student/print' element={<DefaultLayout><PrintingPage/></DefaultLayout>}/>
+          {/* <Route path='/student/print' element={<DefaultLayout><PrintingPage/></DefaultLayout>}/>
           <Route path='/auth' element={<AuthLayout></AuthLayout>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/homepage' element={<Homepage/>} />
           <Route path='/student/history' element={<DefaultLayout><HistoryPage /></DefaultLayout>} />
-          <Route path='/student/buy' element={<DefaultLayout><Purchase /></DefaultLayout>} />                    
+          <Route path='/student/buy' element={<DefaultLayout><Purchase /></DefaultLayout>} />    */}
+          <Route path='/sspo/PrinterManagement' element={<DefaultLayout><PrinterManagement /></DefaultLayout>} />                   
        </Routes>  
     </div>
   )
