4057760a984d09f3bd845870aac94c7dd3114cbe
Hoangth1506 <thhoang150604@gmail.com>
2024-11-27 20:48:43 +0700

    Chỉnh lại css
    căn chỉnh lại các ô pop up với input

--- ssps/src/pages/PagePurchase/FailurePopup.jsx
+++ ssps/src/pages/PagePurchase/FailurePopup.jsx
@@ -3,13 +3,23 @@ import "./Popup.css";
 
 const FailurePopup = ({ onClose }) => {
   return (
-    <div className="popup">
+    <div className="popup-overlay">
       <div className="popup-content">
-        <h2>Giao dịch thất bại!</h2>
-        <p>Vui lòng kiểm tra lại thông tin hoặc nạp thêm tiền.</p>
-        <button onClick={onClose} className="btn-close">
-          Đóng
-        </button>
+        <span className="close-icon" onClick={onClose}>
+          &times;
+        </span>
+        <h2 className="text-danger">GIAO DỊCH THẤT BẠI</h2>
+        <div>
+          <b>
+            Giao dịch không thành công. Vui lòng kiểm tra lại số dư hoặc kết nối
+            mạng!
+          </b>
+        </div>
+        <div style={{ marginTop: "20px" }}>
+          <button className="btn-failure" onClick={onClose}>
+            Đóng
+          </button>
+        </div>
       </div>
     </div>
   );
